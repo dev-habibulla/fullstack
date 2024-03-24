389958d3fa5f8e2b5f3ed2ff9ef4787ee4731aa1
Abdur Rahman Habibulla <habibullalm10@gmail.com>
2024-03-24 21:59:27 +0600

    Create Product Route And  createProductController backend Done

---
+++ backend/controllers/createProductController.js
@@ -0,0 +1,11 @@
+
+
+let createProductController = async (req, res) => {
+  // const { token, password } = req.body;
+
+console.log("createProductController");
+   
+
+};
+
+module.exports = createProductController;
--- backend/routes/api/productRoute.js
+++ backend/routes/api/productRoute.js
@@ -3,11 +3,13 @@ const createCategoryControlar = require("../../controllers/createCategoryControl
 const createSubCategoryControlar = require("../../controllers/createSubCategoryControlar")
 const allCatControlar = require("../../controllers/allCatControlar")
 const allSubCatControlar = require("../../controllers/allSubCatControlar")
+const createProductController = require("../../controllers/createProductController")
 
 const _ = express.Router()
 
 _.post("/createcategory", createCategoryControlar)
 _.post("/createsubcategory", createSubCategoryControlar)
+_.post("/createproduct", createProductController)
 
 _.get("/allcat", allCatControlar)
 _.get("/allsubcat", allSubCatControlar)
